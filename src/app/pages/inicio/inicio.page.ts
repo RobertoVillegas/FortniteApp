f16f63f108b83bf91faa428d04c32cffa247f460
Roberto Villegas <hola@robertovillegas.com>
2020-12-05 23:35:17 -0600

    Noticias en la página de inicio

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -9,6 +9,7 @@ import { FortniteApiService } from 'src/app/services/fortnite-api.service';
 })
 export class InicioPage implements OnInit {
   tienda: any;
+  obj: any;
   daily: Array<any>;
   featured: Array<any>;
   specialFeatured: Array<any>;
@@ -23,8 +24,31 @@ export class InicioPage implements OnInit {
     this.daily = this.tienda['daily']['entries'];
     this.featured = this.tienda['featured']['entries'];
     this.specialFeatured = this.tienda['specialFeatured']['entries'];
+
     console.log(this.daily);
     console.log(this.featured);
     console.log(this.specialFeatured);
+
+    this.obj = await this.fortniteApi.getNews();
+    this.noticias = this.obj['motds'];
+
+    console.log(this.noticias);
+
   }
+
+  //
+  // ─── Slides ───────────────────────────────────────────────────────────────────
+  //
+
+  slideOptsTop = {
+    spaceBetween: 0,
+    slidesPerView: 1.15,
+  };
+  /*
+    slider with more narrow cards, 2 fully visible slides
+  */
+  // slideOptsThumbs = {
+  //   spaceBetween: 0,
+  //   slidesPerView: 2.65,
+  // };
 }
