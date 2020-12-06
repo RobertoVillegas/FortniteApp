2a33eb4c06b5e6e3d01bbada113efa93c1d4d0e5
Roberto Villegas <hola@robertovillegas.com>
2020-12-05 22:22:42 -0600

    Obtención de datos de la tienda

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,15 +7,24 @@ import { FortniteApiService } from 'src/app/services/fortnite-api.service';
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-
 export class InicioPage implements OnInit {
+  tienda: any;
+  daily: Array<any>;
+  featured: Array<any>;
+  specialFeatured: Array<any>;
 
-  noticias: Observable<any>;
+  noticias: any;
 
-  constructor(private fortniteApi: FortniteApiService) { }
+  constructor(private fortniteApi: FortniteApiService) {}
 
-  ngOnInit() {
-    // this.noticias = this.fortniteApi.getNews();
+  async ngOnInit() {
+    this.tienda = await this.fortniteApi.getStore();
+
+    this.daily = this.tienda['daily']['entries'];
+    this.featured = this.tienda['featured']['entries'];
+    this.specialFeatured = this.tienda['specialFeatured']['entries'];
+    console.log(this.daily);
+    console.log(this.featured);
+    console.log(this.specialFeatured);
   }
-
 }
